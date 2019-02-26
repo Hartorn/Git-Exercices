9745ce6de9363316555cc29c86cb09d7619c3872
Bazire <bazire.houssin@dreamquark.com>
2019-02-26 16:54:53 +0100

    feat: add git bisect example

---
+++ ex.11/create.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source ../_generic_create.sh
+
+function _ex() {
+
+
+    touch empty.txt
+    git add .
+    git commit -m "chore: init repo"
+    echo -e "#!/bin/sh\necho OK\n exit 0\n" > test.sh
+    git add .
+    git commit -am "adding tested feature"
+    git tag -a 1.0.0 -m "Version 1.0.0"
+    git push --tags -u origin master
+
+    NB_COMMITS=$(head /dev/urandom | tr -dc 0-9 | head -c 2)
+    echo ${NB_COMMITS}
+    for i in `seq 1 ${NB_COMMITS}`;
+    do
+        head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 >> test.sh
+        git commit -am $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10)
+    done
+
+    echo -e "#!/bin/sh\necho KO\n exit 0\n" > test.sh
+    git commit -am "Breaking everything"
+    NB_COMMITS=$(head /dev/urandom | tr -dc 0-9 | head -c 2)
+
+    for i in `seq 1 ${NB_COMMITS}`;
+    do
+        head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 >> test.sh
+        git commit -am $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10)
+    done
+    git push --tags -u origin master
+
+}
+
+_create _ex
---
+++ ex.11/readme.txt
@@ -0,0 +1,7 @@
+Everything was working fine, but there is a bug in production.
+You can reproduce it, but impossible to determine what is causing this.
+test.sh should return OK, but it is returning KO.
+
+Hopefully, you know that tag 1.0.0 is working fine.
+
+You need to determine what is the first commit causing this bug, to understand this bug.
