f713e286270c73471992f7c1dda04456e19fcd64
Bazire <bazire.houssin@dreamquark.com>
2019-03-05 11:45:51 +0100

    fix(ex.10): using two work directory to add reject with --force-with-lease

--- ex.10/create.sh
+++ ex.10/create.sh
@@ -20,13 +20,22 @@ function _ex() {
     echo "two" > second.txt
     git commit -am "fix: really fixing second.txt"
     git push -u origin feature/add-12345
+
+    cd ..
+    mkdir work_remote
+    cd work_remote
+    git clone ../repo . > /dev/null 2>&1
+    git checkout feature/add-12345
     mv five.txtt five.txt
     git add .
     git commit -m "fix: fixing five.txt filename"
     echo "six" > six.txt
     git commit -am "fix: fixing six.txt content"
     git push -u origin feature/add-12345
-    git reset --hard HEAD~2
+
+    cd ..
+    rm -rf work_remote
+    cd work
     git rm six.txt
     git commit -am "fix: removing unused six.txt"
 
